2104b77f568bf2e3901ca4cb00445670ced7c761
Gabriele Lopez <gabriele.ope@gmail.com>
2026-01-15 08:02:51 +0100

    ls command and fix command echo appearing after command output

--- src/utils/commandParser.ts
+++ src/utils/commandParser.ts
@@ -1,11 +1,13 @@
 import { NavigateFunction } from "react-router";
 import { TerminalStore } from "../stores/useTerminalStore";
+import { fileSystemContent } from "../components/FileSystem/FileSystemContent";
 
 export enum Commands {
   Help = "help",
   Clear = "clear",
   Cls = "cls",
   Login = "login",
+  Ls = "ls",
 }
 
 export default class CommandParser {
@@ -17,11 +19,11 @@ export default class CommandParser {
   }
 
   public parseCommand(command: string, hideCommandEcho: boolean = false): void {
+    let commandOutput = "";
     switch (command.trim().toLowerCase()) {
       case Commands.Help:
-        this.terminalStore.appendToTerminalHistory(
-          "Available commands:\nhelp - Show this help message\nclear - Clear the terminal\nlogin - Requests a username an password to log in to the connected system",
-        );
+        commandOutput =
+          "Available commands:\nhelp - Show this help message\nclear - Clear the terminal\nlogin - Requests a username an password to log in to the connected system\nls - List files in the current directory";
         break;
       case Commands.Clear:
       case Commands.Cls:
@@ -31,14 +33,27 @@ export default class CommandParser {
       case Commands.Login:
         this.navigate("/login");
         break;
+      case Commands.Ls:
+        if (
+          this.terminalStore.directory === "@" ||
+          this.terminalStore.directory === "/"
+        ) {
+          //needs to be :blog and :photos
+          const files = fileSystemContent
+            .map((file) => file.name.replace("/", ":"))
+            .join("\n");
+          commandOutput = files;
+        }
+        break;
       default:
-        this.terminalStore.appendToTerminalHistory(
-          `Unknown command: ${command}\nType 'help' for a list of available commands.`,
-        );
+        commandOutput = `Unknown command: ${command}\nType 'help' for a list of available commands.`;
         break;
     }
     if (!hideCommandEcho) {
-      this.terminalStore.appendToTerminalHistory(`93.43.233.0@>${command}`);
+      this.terminalStore.appendToTerminalHistory(
+        `93.43.233.0${this.terminalStore.directory}>${command}`,
+      );
     }
+    this.terminalStore.appendToTerminalHistory(commandOutput);
   }
 }
